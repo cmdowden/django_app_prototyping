9826d23aaa78264f7d8a3f9e7e9aae6399f97321
cmdowden <c.m.dowden@outlook.com>
2018-12-16 23:37:12 +0000

    setting_up - added first test view with helper function

--- prototype/urls.py
+++ prototype/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from prototype_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test_view/', views.test_view, name='test')
 ]
--- prototype_app/views.py
+++ prototype_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
+from prototype_app.util import test_helper_function
 
-# Create your views here.
+
+def test_view(request):
+    """
+    Go to the /test_view/ URL to trigger this view
+    """
+    result = test_helper_function()
+    return HttpResponse(f'Hello World, check out this groovy result: {result}')
